be5082d0a543a27cf246f09070572484737ea2b8
Karel Klic <kklic@redhat.com>
2012-04-02 10:05:25 +0200

    work in progress

--- lib/AbstractPointer.cpp
+++ lib/AbstractPointer.cpp
@@ -2,62 +2,118 @@
 #include <llvm/Support/raw_ostream.h>
 #include <algorithm>
 
-AbstractPointer::~AbstractPointer()
+namespace AbstractPointer {
+
+Target::Target() : mType(Target::Uninitialized), mIsArrayOffset(false)
 {
-    std::set<AbstractValue*>::const_iterator it = mTargets.begin();
-    for (; it != mTargets.end(); ++it)
-        delete *it;
 }
 
-AbstractPointer* AbstractPointer::clone() const
+void Target::setBlockOffset(size_t offset, Target::Type type)
 {
-    AbstractPointer *dup = new AbstractPointer();
-    std::set<AbstractValue*>::iterator it = mTargets.begin();
-    for (; it != mTargets.end(); ++it)
-        dup->mTargets.insert((*it)->clone());
+    assert(type == GlobalBlock || type == FunctionBlock);
+    mOffset = offset;
+    mType = type;
 }
 
-bool AbstractPointer::operator==(const AbstractValue &value) const
+size_t Target::getBlockOffset() const
+{
+    assert(mType == GlobalBlock || mType == FunctionBlock);
+    return mOffset;
+}
+
+void Target::setVariable(const llvm::Value *variable, Target::Type type)
+{
+    assert(variable && (type == GlobalVariable || type == FunctionVariable));
+    mVariable = variable;
+    mmType = type;
+}
+
+const llvm::Value *Target::getVariable() const
+{
+    assert(mType == GlobalVariable || mType == FunctionVariable);
+    return mVariable;
+}
+
+void Target::setConstant(size_t constant)
+{
+    mConstant = constant;
+    mType = Constant;
+}
+
+size_t Target::getConstant() const
+{
+    assert(mType == Constant);
+    return mConstant;
+}
+
+void Target::setArrayOffset(size_t minOffset, size_t maxOffset)
+{
+    mIsArrayOffset = true;
+    mMinArrayOffset = minOffset;
+    mMaxArrayOffset = maxOffset;
+}
+
+InclusionBased* InclusionBased::clone() const
+{
+    return new InclusionBased(*this);
+}
+
+bool InclusionBased::operator==(const AbstractValue &value) const
 {
     // Check if rhs has the same type.
-    const AbstractPointer *pointer = dynamic_cast<const AbstractPointer*>(&value);
+    const InclusionBased *pointer = dynamic_cast<const InclusionBased*>(&value);
     if (!pointer)
         return false;
+
+    // Check if it has the same number of targets.
+    if (pointer->mTargets.size() != mTargets.size())
+        return false;
+
+    // Check the targets.
+    std::set<Target>::const_iterator it1 = pointer->mTargets.begin(),
+        it2 = mTargets.begin();
+    for (; it2 != mTargets.end(); ++it1, ++it2)
+    {
+        if (*it1 != *it2)
+            return false;
+    }
 
     return true;
 }
 
-void AbstractPointer::merge(const AbstractValue &value)
+void InclusionBased::merge(const AbstractValue &value)
 {
-    const AbstractPointer &vv = dynamic_cast<const AbstractPointer&>(value);
+    const InclusionBased &vv = dynamic_cast<const InclusionBased&>(value);
     std::set<AbstractValue*>::iterator it = vv.mTargets.begin();
     for (; it != vv.mTargets.end(); ++it)
     {
         if (mTargets.find(*it) != mTargets.end())
             continue;
 
-        mTargets.insert((*it)->clone());
+        mTargets.insert(*it);
     }
 }
 
-size_t AbstractPointer::memoryUsage() const
+size_t InclusionBased::memoryUsage() const
 {
     return mTargets.size() * sizeof(AbstractValue*);
 }
 
-bool AbstractPointer::limitMemoryUsage(size_t size)
+bool InclusionBased::limitMemoryUsage(size_t size)
 {
     // Memory usage of this value cannot be lowered.
     return false;
 }
 
-float AbstractPointer::accuracy() const
+float InclusionBased::accuracy() const
 {
     // Not possible to determine.
     return 1.0;
 }
 
-void AbstractPointer::printToStream(llvm::raw_ostream &ostream) const
+void InclusionBased::printToStream(llvm::raw_ostream &ostream) const
 {
-    ostream << "AbstractPointer(size: " << mTargets.size() << "items)";
+    ostream << "InclusionBased(size: " << mTargets.size() << "items)";
 }
+
+} // namespace AbstractPointer
--- lib/AbstractPointer.h
+++ lib/AbstractPointer.h
@@ -4,17 +4,79 @@
 #include "AbstractValue.h"
 #include <set>
 
-class AbstractPointer : public AbstractValue
+namespace AbstractPointer {
+
+// Pointer target -- where the pointer points.
+class Target
 {
 public:
-    std::set<AbstractValue*> mTargets;
+    enum Type {
+        Uninitialized,
+        Constant,
+        GlobalVariable,
+        GlobalBlock,
+        FunctionVariable,
+        FunctionBlock
+    };
+
+    // Initializes the target to the Uninitialized type.
+    Target();
+
+    Type getType() const { return mType; }
+
+    // Set the offset to the memory block.
+    void setBlockOffset(size_t offset, Type type);
+    // Get the offset of the memory block where this target points to.
+    size_t getBlockOffset() const;
+
+    // Set the target to a global or function variable.
+    void setVariable(const llvm::Value *variable, Type type);
+    // Get the global or function variable this target points to.
+    const llvm::Value *getVariable() const;
+
+    // Set the target to a constant memory address, such as NULL or
+    // 0xbaadfood.
+    void setConstant(size_t constant);
+    // Get a constant value the target points to, such as NULL or 0xbaadfood.
+    size_t getConstant() const;
+
+    bool isArrayOffset() const { return mIsArrayOffset; }
+    size_t minArrayOffset() const { return mMinArrayOffset; }
+    size_t maxArrayOffset() const { return mMaxArrayOffset; }
+    void setArrayOffset(size_t minOffset, size_t maxOffset);
+
+protected:
+    Type mType;
+
+    union {
+        // Valid when the target represents a variable.
+        const llvm::Value *mVariable;
+        // Valid when the target represents an anonymous memory block.
+        // This is an offset to OperationalState::mGlobalBlocks or
+        // OperationalState::mFunctionBlocks.
+        size_t mOffset;
+        // A specific constant.
+        size_t mConstant;
+    }
+
+    // If the array offset is valid.
+    bool mIsArrayOffset;
+    // Minimal array offset.
+    size_t mMinArrayOffset;
+    // (size_t)-1 means infinity (all array items).
+    size_t mMaxArrayOffset;
+};
+
+// Inclusion-based flow-insensitive abstract pointer.
+class InclusionBased : public AbstractValue
+{
+public:
+    std::set<Target> mTargets;
 
 public:
-    virtual ~AbstractPointer();
-
     // Implementation of AbstractValue::clone().
     // Covariant return type -- it really overrides AbstractValue::clone().
-    virtual AbstractPointer* clone() const;
+    virtual InclusionBased* clone() const;
     // Implementation of AbstractValue::operator==().
     virtual bool operator==(const AbstractValue &value) const;
     // Implementation of AbstractValue::merge().
@@ -28,5 +90,7 @@ public:
     // Implementation of AbstractValue::printToStream().
     virtual void printToStream(llvm::raw_ostream &ostream) const;
 };
+
+} // namespace AbstractPointer
 
 #endif
