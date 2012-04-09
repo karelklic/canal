f52df531fe287ad379b44dbe143fa26fc98ece7d
Tomas Brukner <xbrukner@fi.muni.cz>
2012-04-09 20:48:46 +0200

    Implementation start.

--- lib/IntEnumeration.h
+++ lib/IntEnumeration.h
@@ -19,6 +19,22 @@ class Enumeration : public AbstractValue
   {
     return new Enumeration<T>(*this);
   }
+
+  //Initialize const value
+  Enumeration(const llvm::Constant* constant) : AbstractValue(constant) {
+      //TODO
+  }
+
+  virtual bool operator==(const AbstractValue& rhs) const {
+      const Enumeration& other = (const Enumeration&) rhs;
+      if (this->Values.size() != other.Values.size()) return false;
+      //TODO - get this done when Enumeration is not template class
+      //for (std::set<T>::const_iterator it = this->Values.begin(), it2 = other.Values.begin();
+      //     it != this->Values.end(); it ++, it2 ++) {
+      //    if (*it != *it2) return false;
+      //}
+      return true;
+  }
 };
 
 } // namespace AbstractInteger
