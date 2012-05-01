585c31740ff1b79d3d190b0274c173059562c483
Tomas Brukner <xbrukner@fi.muni.cz>
2012-05-01 20:44:10 +0200

    Modified to llvm::APInt. Basic support for addition, substraction, multiplication and division.

--- lib/IntRange.h
+++ lib/IntRange.h
@@ -3,35 +3,37 @@
 
 #include "AbstractValue.h"
 #include <llvm/Constants.h>
+#include <llvm/ADT/APInt.h>
 #include <limits>
+
+#define CMP(OP) s ## OP
 
 namespace AbstractInteger {
 
 // Abstracts integer values as a range min - max.
-template <typename T>
 class Range : public AbstractValue
 {
  public:
   bool Empty;
 
   bool NegativeInfinity;
-  T From;
+  llvm::APInt From;
 
   bool Infinity;
-  T To;
+  llvm::APInt To;
 
  public:
   // Initializes to the lowest value.
   Range() : Infinity(false), NegativeInfinity(false), Empty(true) {}
 
   // Covariant return type -- overrides AbstractValue::clone().
-  virtual Range<T> *clone() const
+  virtual Range *clone() const
   {
-    return new Range<T>(*this);
+    return new Range(*this);
   }
 
-  Range(const llvm::Constant* constant) : AbstractValue(constant) {
-      //TODO
+  Range(const llvm::APInt &constant) : From(constant), To(constant) {// : AbstractValue(constant) {
+
   }
 
   virtual bool operator==(const AbstractValue& rhs) const {
@@ -39,8 +41,8 @@ class Range : public AbstractValue
       return this->Empty == other.Empty &&
              this->NegativeInfinity == other.NegativeInfinity &&
               this->Infinity == other.Infinity &&
-              this->From = other.From &&
-              this->To = other.To;
+              this->From == other.From &&
+              this->To == other.To;
   }
 
   virtual void merge(const AbstractValue &v) {
@@ -50,7 +52,7 @@ class Range : public AbstractValue
   }
 
   virtual size_t memoryUsage() const {
-      return 2 * sizeof(T) + 3 * sizeof(bool);
+      return 2 * sizeof(llvm::APInt) + 3 * sizeof(bool);
   }
 
   virtual bool limitMemoryUsage(size_t size) {
@@ -58,10 +60,11 @@ class Range : public AbstractValue
   }
 
   virtual float accuracy() const {
-      return 1.0 - (
-                  (this->Infinity ? std::numeric_limits<T>::max() : this->To) -
-                  (this->NegativeInfinity ? std::numeric_limits<T>::min() : this->From)
-              ) / (std::numeric_limits<T>::max() - std::numeric_limits<T>::min());
+      return 1.0;/* - (
+                  (this->Infinity ? std::numeric_limits<llvm::APInt>::max() : this->To) -
+                  (this->NegativeInfinity ? std::numeric_limits<llvm::APInt>::min() : this->From)
+              ) / (std::numeric_limits<llvm::APInt>::max() - std::numeric_limits<llvm::APInt>::min());*/
+      //TODO
   }
 
   virtual bool isBottom() const {
@@ -83,32 +86,62 @@ class Range : public AbstractValue
   }
 
   void add(const AbstractValue &a, const AbstractValue &b) {
-      const Range<T> &a1 = dynamic_cast<const Range<T> &> (a), &a2 = dynamic_cast<const Range<T> &> (b);
+      const Range &a1 = dynamic_cast<const Range &> (a), &a2 = dynamic_cast<const Range &> (b);
+      this->From = a1.From + a2.From;
+      this->To = a1.To + a2.To;
+  }
+
+  void sub(const AbstractValue &a, const AbstractValue &b) {
+      const Range &a1 = dynamic_cast<const Range &> (a), &a2 = dynamic_cast<const Range &> (b);
+      this->From = a1.From - a2.To;
+      this->To = a1.To - a2.From;
+  }
+
+  void mul(const AbstractValue &a, const AbstractValue &b) {
+      const Range &a1 = dynamic_cast<const Range &> (a), &a2 = dynamic_cast<const Range &> (b);
+      this->From = min(a1.From * a2.From, a1.To * a2.To, a1.From * a2.To, a1.To * a2.From);
+      this->To = max(a1.From * a2.From, a1.To * a2.To, a1.From * a2.To, a1.To * a2.From);
+  }
+
+  void div(const AbstractValue &a, const AbstractValue &b) {
+      const Range &a1 = dynamic_cast<const Range &> (a), &a2 = dynamic_cast<const Range &> (b);
+      this->From = min(a1.From. CMP(div) (a2.From), a1.To. CMP(div) (a2.To), a1.From. CMP(div) (a2.To), a1.To. CMP(div) (a2.From));
+      this->To = max(a1.From. CMP(div) (a2.From), a1.To. CMP(div) (a2.To), a1.From. CMP(div) (a2.To), a1.To. CMP(div) (a2.From));
+  }
+
+  /*void add(const AbstractValue &a, const AbstractValue &b) {
+      const Range &a1 = dynamic_cast<const Range &> (a), &a2 = dynamic_cast<const Range &> (b);
       this->binaryOp(a, b, Range::plus);
   }
 
   void sub(const AbstractValue &a, const AbstractValue &b) {
-      const Range<T> &a1 = dynamic_cast<const Range<T> &> (a), &a2 = dynamic_cast<const Range<T> &> (b);
-      this->binaryOp(a, b, Range::minus);
+      const Range &a1 = dynamic_cast<const Range &> (a), &a2 = dynamic_cast<const Range &> (b);
+      this->binaryOpInverted(a, b, Range::minus);
   }
 
   void mul(const AbstractValue &a, const AbstractValue &b) {
-      const Range<T> &a1 = dynamic_cast<const Range<T> &> (a), &a2 = dynamic_cast<const Range<T> &> (b);
+      const Range &a1 = dynamic_cast<const Range &> (a), &a2 = dynamic_cast<const Range &> (b);
       this->binaryOp(a, b, Range::multiple);
   }
 
   void div(const AbstractValue &a, const AbstractValue &b) {
-      const Range<T> &a1 = dynamic_cast<const Range<T> &> (a), &a2 = dynamic_cast<const Range<T> &> (b);
+      const Range &a1 = dynamic_cast<const Range &> (a), &a2 = dynamic_cast<const Range &> (b);
       this->binaryOp(a, b, Range::divide);
-  }
+  }*/
 
 protected:
+
+#define MIN2(a, b) ( a. CMP(lt) (b) ? a : b )
+#define MAX2(a, b) ( a. CMP(gt) (b) ? a : b )
+
+//For merging
   void setLower(const Range& other) {
       if (this->NegativeInfinity || other.NegativeInfinity) {
           this->NegativeInfinity = true;
       }
       else {
-          this->From = (this->From < other.From ? this->From : other.From);
+          //TODO - Signed or Unsigned
+          this->From = MIN2(this->From, other.From);
       }
   }
 
@@ -117,28 +150,37 @@ protected:
           this->Infinity = true;
       }
       else {
-          this->To = (this->To < other.To ? this->To : other.To);
+          //TODO - Signed or Unsigned
+          this->To = MAX2(this->To, other.To);
       }
   }
 
-  T plus(const T& a, const T& b) const {
+  static const llvm::APInt& min(const llvm::APInt& a, const llvm::APInt& b, const llvm::APInt& c, const llvm::APInt& d) {
+      return MIN2 ( (MIN2(a, b)), (MIN2(c, d)) );
+  }
+
+  static const llvm::APInt& max(const llvm::APInt& a, const llvm::APInt& b, const llvm::APInt& c, const llvm::APInt& d) {
+      return MAX2 ( (MAX2(a, b)), (MAX2(c, d)) );
+  }
+
+/*  llvm::APInt plus(const llvm::APInt& a, const llvm::APInt& b) const {
       return a + b;
   }
 
-  T minus(const T& a, const T& b) const {
+  llvm::APInt minus(const llvm::APInt& a, const llvm::APInt& b) const {
       return a - b;
   }
 
-  T multiple(const T& a, const T& b) const {
+  llvm::APInt multiple(const llvm::APInt& a, const llvm::APInt& b) const {
       return a * b;
   }
 
-  T divide(const T& a, const T& b) const {
+  llvm::APInt divide(const llvm::APInt& a, const llvm::APInt& b) const {
       return a * b;
   }
 
   template <typename F>
-  void binaryOp(const Range<T> &a, const Range<T>& b, F f) {
+  void binaryOp2(const Range &a, const Range& b, F f) {
       if (a.NegativeInfinity || b.NegativeInfinity) this->NegativeInfinity = true;
       else {
           this->From = F(a.From, b.From);
@@ -148,6 +190,18 @@ protected:
           this->To = F(a.To, b.To);
       }
   }
+
+  template <typename F>
+  void binaryOp2Inverted(const Range &a, const Range& b, F f) {
+      if (a.NegativeInfinity || b.NegativeInfinity) this->NegativeInfinity = true;
+      else {
+          this->From = F(a.From, b.To);
+      }
+      if (a.Infinity || b.Infinity) this->Infinity = true;
+      else {
+          this->To = F(a.To, b.From);
+      }
+  }*/
 };
 
 } // namespace AbstractInteger
