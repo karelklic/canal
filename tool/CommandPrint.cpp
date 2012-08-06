1b30a227adce96806660d9d09a08cd2c29eb371d
Karel Klic <kklic@redhat.com>
2012-08-06 19:17:10 +0200

    printing command completion and output using the new notation of four kinds of memory

--- tool/CommandPrint.cpp
+++ tool/CommandPrint.cpp
@@ -20,8 +20,37 @@ CommandPrint::CommandPrint(Commands &commands)
 {
 }
 
+static void
+filterPlaceValueMap(const Canal::PlaceValueMap &map,
+                    Canal::SlotTracker &slotTracker,
+                    const char *prefix,
+                    const std::string &arg,
+                    bool addFunctionName,
+                    std::vector<std::string> &result)
+{
+    Canal::PlaceValueMap::const_iterator it = map.begin();
+    for (; it != map.end(); ++it)
+    {
+        std::stringstream name;
+        name << prefix;
+        if (addFunctionName)
+        {
+            const llvm::Instruction &instruction =
+                llvm::cast<llvm::Instruction>(*it->first);
+            const llvm::Function &f = *instruction.getParent()->getParent();
+            name << f.getName().str() << ":";
+        }
+
+        name << Canal::getName(*it->first, slotTracker);
+        if (0 == strncmp(name.str().c_str(), arg.c_str(), arg.size()))
+            result.push_back(name.str());
+    }
+}
+
 std::vector<std::string>
-CommandPrint::getCompletionMatches(const std::vector<std::string> &args, int pointArg, int pointArgOffset) const
+CommandPrint::getCompletionMatches(const std::vector<std::string> &args,
+                                   int pointArg,
+                                   int pointArgOffset) const
 {
     std::vector<std::string> result;
     State *state = mCommands.getState();
@@ -29,31 +58,107 @@ CommandPrint::getCompletionMatches(const std::vector<std::string> &args, int poi
         return result;
 
     std::string arg(args[pointArg].substr(0, pointArgOffset));
-    const llvm::Function &function = state->getStack().getCurrentFunction();
-    state->getSlotTracker().setActiveFunction(function);
 
-    // Check function arguments.
-    llvm::Function::ArgumentListType::const_iterator it = function.getArgumentList().begin();
-    for (; it != function.getArgumentList().end(); ++it)
-        considerCompletionMatch(*it, result, arg);
+    Canal::State &curState = state->getStack().getCurrentState();
 
-    // Check every instruction in the current function.
-    llvm::Function::const_iterator fit = function.begin(), fitend = function.end();
-    for (; fit != fitend; ++fit)
-    {
-        llvm::BasicBlock::const_iterator bit = fit->begin(), bitend = fit->end();
-        for (; bit != bitend; ++bit)
-            considerCompletionMatch(*bit, result, arg);
-    }
-
-    // Check named and unnamed module-level variables.
-    const llvm::Module &module = state->getModule();
-    for (llvm::Module::const_global_iterator it = module.global_begin(); it != module.global_end(); ++it)
-    {
-        considerCompletionMatch(*it, result, arg);
-    }
+    filterPlaceValueMap(curState.getGlobalVariables(),
+                        state->getSlotTracker(),
+                        "@", arg, false, result);
+    filterPlaceValueMap(curState.getGlobalBlocks(),
+                        state->getSlotTracker(),
+                        "@^", arg, true, result);
+    filterPlaceValueMap(curState.getFunctionVariables(),
+                        state->getSlotTracker(),
+                        "%", arg, false, result);
+    filterPlaceValueMap(curState.getFunctionBlocks(),
+                        state->getSlotTracker(),
+                        "%^", arg, false, result);
 
     return result;
+}
+
+static void
+printVariable(const std::string &fullName, State &state)
+{
+    if (fullName.size() == 0)
+        return;
+
+    if ((fullName[0] != '%' && fullName[0] != '@') ||
+        fullName.size() == 1 ||
+        (fullName.size() == 2 && fullName[1] == '^'))
+    {
+        printf("Parameter \"%s\": invalid format.\n", fullName.c_str());
+        return;
+    }
+
+    bool isBlock = (fullName.size() > 1 && fullName[1] == '^');
+
+    const llvm::Value *position;
+    bool isNumber;
+    std::string name(fullName.substr(isBlock ? 2 : 1));
+
+    std::string functionName;
+    if (isBlock)
+    {
+        size_t sep = name.find(":");
+        if (std::string::npos != sep)
+        {
+            functionName = name.substr(0, sep);
+            name = name.substr(sep + 1);
+        }
+    }
+
+    unsigned pos = stringToUnsigned(name.c_str(), isNumber);
+
+    Canal::Stack &stack = state.getStack();
+    Canal::State &currentState = stack.getCurrentState();
+    Canal::SlotTracker &slotTracker = state.getSlotTracker();
+
+    const llvm::Function *function = &stack.getCurrentFunction();
+    if (!functionName.empty())
+    {
+        function = state.getModule().getFunction(functionName);
+        CANAL_ASSERT_MSG(function, "Cannot found already used function!");
+    }
+    slotTracker.setActiveFunction(*function);
+
+    if (isNumber)
+    {
+        if (fullName[0] == '%' || isBlock)
+            position = slotTracker.getLocalSlot(pos);
+        else
+            position = slotTracker.getGlobalSlot(pos);
+    }
+    else
+    {
+        if (fullName[0] == '%' || isBlock)
+            position = function->getValueSymbolTable().lookup(name);
+        else
+            position = state.getModule().getValueSymbolTable().lookup(name);
+    }
+
+    if (!position)
+    {
+        printf("Parameter \"%s\": does not exist in current scope.\n",
+               fullName.c_str());
+        return;
+    }
+
+    Canal::Value *value = NULL;
+    if (isBlock)
+        value = currentState.findBlock(*position);
+    else
+        value = currentState.findVariable(*position);
+
+    if (!value)
+    {
+        printf("%s = uninitialized\n", fullName.c_str());
+        return;
+    }
+
+    printf("%s = %s\n", fullName.c_str(),
+           Canal::indentExceptFirstLine(value->toString(),
+                                        fullName.size() + 3).c_str());
 }
 
 void
@@ -72,65 +177,7 @@ CommandPrint::run(const std::vector<std::string> &args)
         return;
     }
 
-    Canal::Stack &stack = state->getStack();
-    Canal::State &currentState = stack.getCurrentState();
-    const llvm::Function &function = stack.getCurrentFunction();
-    Canal::SlotTracker &slotTracker = state->getSlotTracker();
-    slotTracker.setActiveFunction(function);
-
-    for (std::vector<std::string>::const_iterator it = args.begin() + 1; it != args.end(); ++it)
-    {
-        if (it->size() == 0)
-            continue;
-        if (((*it)[0] != '%' && (*it)[0] != '@') || it->size() == 1)
-        {
-            printf("Parameter \"%s\": invalid format.\n", it->c_str());
-            continue;
-        }
-
-        const llvm::Value *position;
-        bool isNumber;
-        std::string name(it->substr(1));
-        unsigned pos = stringToUnsigned(name.c_str(), isNumber);
-        if (isNumber)
-        {
-            if ((*it)[0] == '%')
-                position = slotTracker.getLocalSlot(pos);
-            else
-                position = slotTracker.getGlobalSlot(pos);
-        }
-        else
-        {
-            if ((*it)[0] == '%')
-                position = function.getValueSymbolTable().lookup(name);
-            else
-                position = state->getModule().getValueSymbolTable().lookup(name);
-        }
-
-        if (!position)
-        {
-            printf("Parameter \"%s\": does not exist in current scope.\n", it->c_str());
-            continue;
-        }
-
-        Canal::Value *value = currentState.findVariable(*position);
-        if (!value)
-        {
-            printf("%s = uninitialized\n", it->c_str());
-            continue;
-        }
-
-        printf("%s = %s\n", it->c_str(), Canal::indentExceptFirstLine(value->toString(&currentState), it->size() + 3).c_str());
-    }
-}
-
-void
-CommandPrint::considerCompletionMatch(const llvm::Value &value, std::vector<std::string> &result, const std::string &prefix) const
-{
-    std::string name(Canal::getName(value, mCommands.getState()->getSlotTracker()));
-    if (name.empty())
-        return;
-
-    if (0 == strncmp(name.c_str(), prefix.c_str(), prefix.size()))
-        result.push_back(name);
+    std::vector<std::string>::const_iterator it = args.begin() + 1;
+    for (; it != args.end(); ++it)
+        printVariable(*it, *state);
 }
--- tool/CommandPrint.h
+++ tool/CommandPrint.h
@@ -13,12 +13,13 @@ public:
     CommandPrint(Commands &commands);
 
     // Implementation of Command::getCompletionMatches().
-    virtual std::vector<std::string> getCompletionMatches(const std::vector<std::string> &args, int pointArg, int pointArgOffset) const;
+    virtual std::vector<std::string> getCompletionMatches(
+        const std::vector<std::string> &args,
+        int pointArg,
+        int pointArgOffset) const;
+
     // Implementation of Command::run().
     virtual void run(const std::vector<std::string> &args);
-
-protected:
-    void considerCompletionMatch(const llvm::Value &value, std::vector<std::string> &result, const std::string &prefix) const;
 };
 
 #endif // CANAL_COMMAND_PRINT_H
