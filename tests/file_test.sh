1390884bdec08c76739518c8eedc0357f0b10d4a
Tomas Brukner <xbrukner@fi.muni.cz>
2012-11-10 23:31:07 +0100

    Fixed file test to correctly handle exit status of Canal and call it with --no-missing.

--- tests/file_test.sh
+++ tests/file_test.sh
@@ -8,11 +8,14 @@ if [ $# -ne 2 ]; then
 fi
 
 ulimit -t $TIME
-$1 $2 -e run -e quit
-
+$1 $2 --no-missing -e run -e quit
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+	exit $exit_status
+fi
 filename="${2##*/}"
 if [ ! -f $filename ]; then
 	echo "Compilation failed"
 	exit 3
 fi
-exit $?
+exit 0
