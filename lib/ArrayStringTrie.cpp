5d0bf7a031d2c4fb6b86b5b730626c1d26e01d90
Matej Suta <suta.matej@gmail.com>
2013-02-16 20:09:27 +0100

    added string trie constructors inspired by StringPrefix class

--- lib/ArrayStringTrie.cpp
+++ lib/ArrayStringTrie.cpp
@@ -1,14 +1,21 @@
 #include "ArrayStringTrie.h"
 #include "Environment.h"
 #include "Utils.h"
+#include "IntegerUtils.h"
 
 namespace Canal {
 namespace Array {
+
+TrieNode::TrieNode(const std::string &value)
+    : mValue(value)
+{ 
+}
 
 StringTrie::StringTrie(const Environment &environment,
                        const llvm::SequentialType &type)
     : Domain(environment, Domain::ArrayStringTrieKind),
       mIsBottom(true),
+      mRoot(new TrieNode("")),
       mType(type)
 {
     const llvm::Type *int8 = llvm::Type::getInt8Ty(environment.getContext());
@@ -16,10 +23,66 @@ StringTrie::StringTrie(const Environment &environment,
         setTop();
 }
 
+StringTrie::StringTrie(const Environment &environment,
+                       const llvm::SequentialType &type,
+                       std::vector<Domain*>::const_iterator begin,
+                       std::vector<Domain*>::const_iterator end)
+    : Domain(environment, Domain::ArrayStringTrieKind),
+      mIsBottom(true),
+      mRoot(new TrieNode("")),
+      mType(type)
+{
+    const llvm::Type *int8 = llvm::Type::getInt8Ty(environment.getContext());
+    if (mType.getElementType() != int8)
+        setTop();
+    else
+    {
+        TrieNode *newNode = new TrieNode("");
+
+        std::vector<Domain*>::const_iterator it = begin;
+        for (; it != end; ++it)
+        {
+            if (!Integer::Utils::isConstant(**it))
+                break;
+
+            CANAL_ASSERT_MSG(8 == Integer::Utils::getBitWidth(**it),
+                             "String requires 8-bit characters.");
+
+            llvm::APInt constant;
+            bool success = Integer::Utils::signedMin(**it, constant);
+            CANAL_ASSERT(success);
+
+            uint64_t c = constant.getZExtValue();
+            if (c == 0 || c > 255)
+                break;
+
+            mIsBottom = false;
+            newNode->mValue.append(1, char(c));
+        }
+
+        if (!mIsBottom)
+        {
+            mRoot->mChildren.push_back(newNode);
+        }
+    }
+}
+
+StringTrie::StringTrie(const Environment &environment,
+                       const std::string &value)
+    : Domain(environment, Domain::ArrayStringTrieKind),
+      mIsBottom(false),
+      mRoot(new TrieNode("")),
+      mType(*llvm::ArrayType::get(llvm::Type::getInt8Ty(environment.getContext()),
+                                  value.size()))
+{
+    TrieNode *newNode = new TrieNode(value);
+    mRoot->mChildren.push_back(newNode);
+}
+
 StringTrie *
 StringTrie::clone() const
 {
-    CANAL_NOT_IMPLEMENTED();
+    return new StringTrie(*this);
 }
 
 size_t
